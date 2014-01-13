031a3a123d5f81e7e6413d97b8089563db5af0c0
mhlinder <mhlinder@gmail.com>
2014-01-13 10:55:35 -0500

    starting d3.js placement of text elements; python will automate this

---
+++ Readme.md
@@ -0,0 +1,15 @@
+Tufte Slopegraphs
+=================
+
+A D3.js, LaTeX take on [this Edward Tufte challenge]{http://www.edwardtufte.com/bboard/q-and-a-fetch-msg?msg_id=0003nk}
+
+`slopegraph.py` will generate a D3.js document and a LaTeX file containing slope graphs of input data, with a specified title.
+
+Use
+---
+User specifies two items in `slopegraph.py`: `filename` and `title`.
+
+File specifies a source data file, which is comma separated with three columns:
+1. `label`: Item label
+2. `col1`: Column 1, titled "col1"
+3. `col2`: Column 2, title "col2"
--- slopegraph.py
+++ slopegraph.py
@@ -1,8 +1,19 @@
 # http://www.edwardtufte.com/bboard/q-and-a-fetch-msg?msg_id=0003nk
 
-f = open('gnp.csv','r')
-lines = f.readlines()
-f.close()
+def json_csv(filename):
+    f = open(filename, 'r')
+    lines = f.readlines()
+    f.close()
 
-head = lines[0].rstrip().split(',')
-rows = [l.rstrip().split(',') for l in lines[1:]]
+    head = lines[0].rstrip().split(',')
+    rows = [l.rstrip().split(',') for l in lines[1:]]
+
+    json = []
+    for row in rows:
+        row = [row[0], float(row[1]), float(row[2])]
+        json.append(dict(zip(head,row)))
+    return json
+
+filename = "gnp.csv"
+title = "Current Receipts of Government as a Percentage of Gross Domestic Product, 1970 and 1979"
+data = json_csv(filename)
